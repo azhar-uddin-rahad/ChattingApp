89a439237dee71a6f3f0cb225fcbc6832feeff8a
MamunHowlader704 <azheruddinrahad@gmail.com>
2023-11-26 23:24:54 +0600

    chatting app dark mood and white mood done

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,10 @@ import RootLayout from "./RootLayout/RootLayout";
 import Msg from "./Pages/Msg";
 import Notification from "./Pages/Notification";
 import Setting from "./Pages/Setting";
+import Switch from '@mui/material/Switch';
+import { useState } from "react";
 function App() {
- 
+ const [dark,setDark]=useState(false);
   const route = createBrowserRouter(
     createRoutesFromElements(
       <Route>
@@ -31,10 +33,24 @@ function App() {
       </Route>
     )
   );
+  const handleTheme=(e)=>{
+    console.log(e.target.value);
+    if(dark){
+      setDark(false)
+      console.log("light")
+    }
+    else{
+      setDark(true)
+     
+      console.log("dark")
+    }
+ }
 
   return <div className="App">
-  
+   <Switch onChange={(e)=>{handleTheme(e)}} className="switchBtn" />
+  <div className={`${dark ? "dark" : ""}`}>
   <RouterProvider router={route}></RouterProvider>
+  </div>
   <ToastContainer />
   </div>;
 }
